8c8cc21bcc4ab732b85cea5eecc95bb4e64cf4f3
afonso <alvesafonso12@gmail.com>
2018-05-15 15:12:17 +0100

    teste

--- src/sti/Main.java
+++ src/sti/Main.java
@@ -3,7 +3,10 @@ package sti;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         ///stu
+
+    }
+    public void cenas(int a , int b) {
     }
 }
